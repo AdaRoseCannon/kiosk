6830c0caa4ba5dd28e77a47b65812df11e3b0e19
Ada Rose Cannon <ada@ada.is>
2021-01-03 20:44:35 +0000

    Add new title animations

--- main.js
+++ main.js
@@ -1,70 +1,93 @@
-const unassignedElements = Array.from(document.querySelectorAll('body > [id]'));
-const state = new Map();
-const fade = [
-	[
-		// keyframes
-		{
-			opacity: '1',
-		},
-		{
-			opacity: '0',
-		},
-	],
-	{
-		// timing options
-		duration: 1000,
-	},
-];
-
-const probablyRelease = new Set();
-async function midiMessage(msg) {
-	const input = msg.currentTarget;
-	const device = (input.manufacturer ? input.manufacturer + ', ' : '') + (input.name ? input.name : 'unnamed device');
-	const data = msg.data;
-	let channel = data[0];
-	let release = false;
-	const note = data[1];
-	let type = 'cc';
-
-	if (probablyRelease.has(channel)) {
-		release = true;
-		channel += 16;
-	}
-
-	// send type as well - if channel is between certain numbers then it's a pad, if not it's a CC - or something (it's a little bit rudementary but I don't have a better way ya)
-	if (channel > 120 && channel < 150) {
-		type = 'pad';
-	}
-
-	probablyRelease.add(channel - 16);
-
-	const key = `${device}: ${type}, channel: ${channel}, note: ${note}`;
-	console.log(key);
-
-	const el = (state.has(key) && document.getElementById(state.get(key))) || unassignedElements.shift();
-	state.set(key, el.id);
-
-	el.getAnimations().map(animation => animation.cancel());
-
-	if (!release) {
-		el.style.display = 'block';
-		if (el.adaAnimation) {
-			const anim = el.animate(...el.adaAnimation);
-			await anim.finished.catch(e => console.log(e));
-		}
-	}
-
-	if (release) {
-		if (el.adaExitAnimation) {
-			const exitAnim = el.animate(...(el.adaExitAnimation));
-			await exitAnim.finished.catch(e => console.log(e));
-		}
-		el.style.display = 'none';
-	}
-}
-
 (async function init() {
-	const access = await navigator.requestMIDIAccess();
+
+	const unassignedElements = Array.from(document.querySelectorAll('body > [id]'));
+	const data = (await localforage.getItem('state')) || [];
+	const state = new Map(data);
+
+	const fade = [
+		[
+			// keyframes
+			{
+				opacity: '1',
+			},
+			{
+				opacity: '0',
+			},
+		],
+		{
+			// timing options
+			duration: 1000,
+		},
+	];
+
+	function updateState() {
+		const data = Array.from(state.entries());
+		localforage.setItem('state', data);
+	}
+
+	const probablyRelease = new Set();
+	async function midiMessage(msg) {
+		const input = msg.currentTarget;
+		const device = (input.manufacturer ? input.manufacturer + ', ' : '') + (input.name ? input.name : 'unnamed device');
+		const data = msg.data;
+		let channel = data[0];
+		let release = false;
+		const note = data[1];
+		let type = 'cc';
+
+		if (probablyRelease.has(channel)) {
+			release = true;
+			channel += 16;
+		}
+
+		// send type as well - if channel is between certain numbers then it's a pad, if not it's a CC - or something (it's a little bit rudementary but I don't have a better way ya)
+		if (channel > 120 && channel < 150) {
+			type = 'pad';
+		}
+
+		probablyRelease.add(channel - 16);
+
+		const key = `${device}: ${type}, channel: ${channel}, note: ${note}`;
+		console.log(key);
+
+		const el = (state.has(key) && document.getElementById(state.get(key))) || unassignedElements.shift();
+
+		if (!el) return;
+
+		state.set(key, el.id);
+		updateState();
+
+		el.getAnimations().map(animation => animation.cancel());
+
+		if (!release) {
+			el.style.display = 'block';
+			if (el.adaAnimation) {
+				const anim = el.animate(...el.adaAnimation);
+				await anim.finished.catch(e => console.log(e));
+			}
+
+			if (el.adaFunctionIn) {
+				el.adaFunctionIn();
+			}
+		}
+
+		if (release) {
+			if (el.adaExitAnimation) {
+				const exitAnim = el.animate(...(el.adaExitAnimation));
+				await exitAnim.finished.catch(e => console.log(e));
+				el.style.display = 'none';
+			} else if (el.adaFunctionOut) {
+				el.adaFunctionOut();
+				return;
+			} else {
+				el.style.display = 'none';
+			}
+		}
+	}
+
+	const access = await navigator.requestMIDIAccess({
+		sysex: false
+	});
 
 	// Get lists of available MIDI controllers
 	const inputs = access.inputs.values();
@@ -80,4 +103,4 @@ async function midiMessage(msg) {
 		console.log(e.port.name, e.port.manufacturer, e.port.state);
 	};
 
-}());
+}())
